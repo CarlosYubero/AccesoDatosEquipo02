06e4d2d3f9828f3a662297266a48de634d979ca2
pc <pc@CHUFASCHUFURIUS>
2014-10-30 16:51:37 +0100

    JavaBeans Employee y Departament

--- AccesoDatos/src/ut01/act06/Deparment.java
+++ AccesoDatos/src/ut01/act06/Deparment.java
@@ -1,14 +1,68 @@
 package ut01.act06;
 import java.io.Serializable;
 
-public class Deparment {
+public class Deparment implements Serializable {
 
-	private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1V;
+	private int id;
+	private String lastname;
+	private String name;
+	private String job;
+	private date regis_date;
+	private int comission;
+	private int dep_number;	
 
 	public Deparment() {
-
 	}
-
+	
+	public void setId(int id){
+		this.id=id;
+	}
+	
+	public void setLastName(String lastname){
+		this.lastname=lastname;
+	}
+	
+	public void setName(String name){
+		this.name=name;
+	}
+	
+	public void setJob(String job){
+		this.job=job;
+	}
+	
+	public void setComission(int comission){
+		this.comission=comission;
+	}
+	
+	public void setDepNumber(int dep_number){
+		this.dep_number=dep_number;
+	}
+	
+	public int getId(){
+		return id;
+	}
+	
+	public int getLastName(){
+		return lastname;
+	}
+	
+	public int getName(){
+		return name;
+	}
+	
+	public int getJob(){
+		return job;
+	}
+	
+	public int getComission(){
+		return comission;
+	}
+	
+	public int getDepNumber(){
+		return dept_number;
+	}
+	
 	@Override
 	public boolean equals(Object obj) {
 		return super.equals(obj);
--- AccesoDatos/src/ut01/act06/Employee.java
+++ AccesoDatos/src/ut01/act06/Employee.java
@@ -3,14 +3,39 @@ package ut01.act06;
 import java.io.Serializable;
 import java.util.GregorianCalendar;
 
-public class Employee implements Serializable {
+private static final long serialVersionUID = 1V;
+private int id;
+private String name;
+private String head;
 
 
-	private static final long serialVersionUID = 1L;
+public Employee() {
+}
 
-	public Employee() {
+public void setId(int id){
+	this.id=id;
+}
 
-	}
+public void setName(String name){
+	this.name=name;
+}
+
+public void setHead(String head){
+	this.head=head;
+}
+
+public int getId(){
+	return id;
+}
+
+public int getName(){
+	return name;
+}
+
+public int getHead(){
+	return head;
+}
+
 
 	@Override
 	public boolean equals(Object obj) {
